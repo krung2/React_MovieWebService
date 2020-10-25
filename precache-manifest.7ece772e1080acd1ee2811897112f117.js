c06e1581871318816a4de33cb60ac968ff349ab4
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-10-25 21:45:20 +0900

    Updates

---
+++ precache-manifest.7ece772e1080acd1ee2811897112f117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa56c5a29b62a80783edd377316829d",
+    "url": "//krung2.github.io/react_movie_web_service/index.html"
+  },
+  {
+    "revision": "618e0f84616d2ec116b2",
+    "url": "//krung2.github.io/react_movie_web_service/static/css/main.f8701b45.chunk.css"
+  },
+  {
+    "revision": "2eebf0b1b99ad1eae22a",
+    "url": "//krung2.github.io/react_movie_web_service/static/js/2.ab0326fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "//krung2.github.io/react_movie_web_service/static/js/2.ab0326fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618e0f84616d2ec116b2",
+    "url": "//krung2.github.io/react_movie_web_service/static/js/main.186c21c0.chunk.js"
+  },
+  {
+    "revision": "9d5a79dea3445fd22099",
+    "url": "//krung2.github.io/react_movie_web_service/static/js/runtime-main.7a474418.js"
+  }
+]);
